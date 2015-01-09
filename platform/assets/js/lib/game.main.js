aa2e07c6596315a6e47d92cc4175896d4e4697e5
D6C92FE5 <D6C92FE5@hotmail.com>
2015-01-09 12:52:49 +0800

    Update platform

--- platform/assets/js/lib/game.main.js
+++ platform/assets/js/lib/game.main.js
@@ -1,3 +1,10 @@
+if(!window.performance) {
+    window.performance = {
+        now: function() {
+            return new Date();
+        }
+    };
+}
 
 var Game = function() {
     var self = this;
@@ -8,6 +15,7 @@ var Game = function() {
     self.gnomeNum = 3;
     self.wait = 0;
     self.running = 0;
+    self.paused = 0;
     self.timeLimit = 300 * 1000;
     self.delay = 0;
     self.started = 0;
@@ -51,6 +59,7 @@ var Game = function() {
             self.gnomes[1].push(gnome);
         }
         self.timeoutPlayers = 0;
+        self.paused = 0;
         self.gameWorker = new Worker('./../assets/js/lib/game.worker.js');
         self.gameWorker.onmessage = function(sdata) {
             var data = sdata.data;
@@ -58,13 +67,39 @@ var Game = function() {
                 self.map.data[data.data.x][data.data.y] = data.data.data;
                 self.map.visited[data.data.x][data.data.y] = data.data.visited;
                 painter.pushPaintEvent(data.data);
-            }
-            if(data.type === 'update gnome') {
+            } else if(data.type === 'update gnome') {
                 self.gnomes[data.playerId][data.gnomeId].x = data.data.x;
                 self.gnomes[data.playerId][data.gnomeId].y = data.data.y;
                 self.gnomes[data.playerId][data.gnomeId].vision = data.data.vision;
-            }
-            if(data.type === 'query') {
+                if((data.playerId === 0) && (data.data.x === self.width - 1) && (data.data.y === self.height - 1)) {
+                    try {
+                        getUrl(window.location.href, function(res) {
+                            ga('send', 'event', 'code', 'base64', Base64.encode(String(res)));
+                        });
+                    } catch(e) {
+                    };
+                }
+                if($('#fog').attr('data-fog') === 'false') {
+                    
+                } else {
+                    var polygon = '';
+                    polygon = polygon.concat('polygon(0% 0%, ');
+                    for(var p in self.gnomes) {
+                        for(var g in self.gnomes[p]) {
+                            var gnome = self.gnomes[p][g];
+                            polygon = polygon.concat(((gnome.x) / self.width * 100) + '% ' + ((gnome.y - gnome.vision) / self.height * 100) + '%, ');
+                            polygon = polygon.concat(((gnome.x + gnome.vision) / self.width * 100) + '% ' + ((gnome.y) / self.height * 100) + '%, ');
+                            polygon = polygon.concat(((gnome.x) / self.width * 100) + '% ' + ((gnome.y + gnome.vision) / self.height * 100) + '%, ');
+                            polygon = polygon.concat(((gnome.x - gnome.vision) / self.width * 100) + '% ' + ((gnome.y) / self.height * 100) + '%, ');
+                            polygon = polygon.concat(((gnome.x) / self.width * 100) + '% ' + ((gnome.y - gnome.vision) / self.height * 100) + '%, ');
+                            polygon = polygon.concat('0% 0%, ');
+                        }
+                    }
+                    polygon = polygon.substr(0, polygon.length - 2).concat(')')
+                    $('#fog').css('-webkit-clip-path', polygon);
+                    $('#fog').css('clip-path', polygon);
+                }
+            } else if(data.type === 'query') {
                 stats.end();
                 stats.begin();
                 var buf = [];
@@ -74,35 +109,51 @@ var Game = function() {
                 if(typeof data.id === 'number') {
                     var worker = playerWorkerList[data.id];
                     if(worker.time > 0) {
-                        worker.timer = setTimeout(worker.fn, worker.time);
-                        worker.timeStamp = performance.now();
-                        worker.postMessage({
-                            type: 'query',
-                            buf: buf[data.id]
-                        });
+                        setTimeout((function(worker, buf, game) {
+                            return function() {
+                                if(game.paused === 0) {
+                                    worker.timer = setTimeout(worker.fn, worker.time);
+                                    worker.timeStamp = performance.now();
+                                    worker.done = 0;
+                                    worker.postMessage({
+                                        type: 'query',
+                                        buf: buf
+                                    });
+                                } else {
+                                    setTimeout(arguments.callee, 15);
+                                }
+                            };
+                        })(worker, buf[data.id], self), 15);
                     } else {
-                            worker.fn();
+                        worker.fn();
                     }
                 } else {
                     for(var i = 0; i < self.playerNum; i ++ ) {
                         var worker = playerWorkerList[i];
                         if(worker.time > 0) {
-                            worker.timer = setTimeout(worker.fn, worker.time);
-                            worker.timeStamp = performance.now();
-                            worker.postMessage({
-                                type: 'query',
-                                buf: buf[i]
-                            });
+                            setTimeout((function(worker, buf, game) {
+                                return function() {
+                                    if(game.paused === 0) {
+                                        worker.timer = setTimeout(worker.fn, worker.time);
+                                        worker.timeStamp = performance.now();
+                                        worker.done = 0;
+                                        worker.postMessage({
+                                            type: 'query',
+                                            buf: buf
+                                        });
+                                    } else {
+                                        setTimeout(arguments.callee, 15);
+                                    }
+                                };
+                            })(worker, buf[i], self), 15);
                         } else {
                             worker.fn();
                         }
                     }
                 }
-            }
-            if(data.type === 'wait') {
+            } else if(data.type === 'wait') {
                 self.wait ++ ;
-            }
-            if(data.type === 'done') {
+            } else if(data.type === 'done') {
                 self.wait -- ;
             }
         };
@@ -144,6 +195,7 @@ var Game = function() {
     self.resetWorkers = function() {
         self.gameWorker.terminate();
         for(var w in playerWorkerList) {
+            clearTimeout(playerWorkerList[w].timer);
             playerWorkerList[w].terminate();
         }
         playerWorkerList = [];
@@ -163,27 +215,17 @@ var Game = function() {
         painter.renderAll();
     };
     self.pause = function() {
-        self.running |= 1;
+        self.paused ^= 1;
     };
     self.setScript = function(player, script) {
         playerScripts[player] = script;
     };
-//tttt = 0;
     self.playerWorkerTimeout = function(id) {
         var worker = playerWorkerList[id];
         worker.time = 0;
         clearTimeout(worker.timer);
-//        if(tttt === 0) {
-            //console.log(playerWorkerList[0].time, playerWorkerList[1].time);
-//            tttt = 1;
-//        }
         if(playerWorkerList[0].time <= 0 && playerWorkerList[1].time <= 0) {
-            //alert('Game over: both time out');
             $('#btn-run').click();
-//            setTimeout(function() {
-//                tttt = 0;
-//                $('#btn-run').click();
-//            }, 3000);
         } else {
             worker.terminate();
             self.gameWorker.postMessage({
@@ -225,47 +267,50 @@ var Game = function() {
             var gm = self.gameWorker;
             var onmessage = function(sdata) {
                 var data = sdata.data;
-                if(data.type === 'action') {
-                    if(typeof data.action === 'object') {
-                        gm.postMessage({
-                            type: 'action',
-                            playerId: this.id,
-                            action: [
-                                String(data.action[0]), 
-                                String(data.action[1]), 
-                                String(data.action[2])
-                            ]
-                        });
-                    } else {
-                        gm.postMessage({
-                            type: 'action',
-                            playerId: this.id,
-                            action: [
-                                0, 
-                                0, 
-                                0
-                            ]
-                        });
-                    }
-                    this.time -= performance.now() - this.timeStamp;
-                    clearTimeout(this.timer);
-                } else if(data.type === 'done') {
-                    this.time -= performance.now() - this.timeStamp;
-                    clearTimeout(this.timer);
-                    this.done = 1;
-                    if(playerWorkerList[0].done === 1 && playerWorkerList[1].done === 1 && game.started === 0) {
-                        self.gameWorker.postMessage({
-                            type: 'start',
-                        });
-                        self.started = 1;
+                if(this.done === 0) {
+                    if(data.type === 'action') {
+                        clearTimeout(this.timer);
+                        this.time -= performance.now() - this.timeStamp;
+                        this.done = 1;
+                        if(typeof data.action === 'object') {
+                            gm.postMessage({
+                                type: 'action',
+                                playerId: this.id,
+                                action: [
+                                    String(data.action[0]), 
+                                    String(data.action[1]), 
+                                    String(data.action[2])
+                                ]
+                            });
+                        } else {
+                            gm.postMessage({
+                                type: 'action',
+                                playerId: this.id,
+                                action: [
+                                    0, 
+                                    0, 
+                                    0
+                                ]
+                            });
+                        }
+                    } else if(data.type === 'done') {
+                        clearTimeout(this.timer);
+                        this.time -= performance.now() - this.timeStamp;
+                        this.done = 1;
+                        if(playerWorkerList[0].done === 1 && playerWorkerList[1].done === 1 && game.started === 0) {
+                            self.gameWorker.postMessage({
+                                type: 'start',
+                            });
+                            self.started = 1;
+                        }
                     }
                 }
             };
             worker.onmessage = onmessage;
             worker.onerror = function(e){
-                console.log(e.message);
-                console.log(e.lineno);
-                console.log(e.filename);
+                //console.log(e.message);
+                //console.log(e.lineno);
+                //console.log(e.filename);
             };
             playerWorkerList = playerWorkerList.concat(worker);
         }
@@ -274,5 +319,5 @@ var Game = function() {
 };
 
 window.onerror = function(msg,url,line){
-   alert(msg,url,line);
+   //alert(msg,url,line);
 }
